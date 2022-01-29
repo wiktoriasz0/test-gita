6dbfeb15829a1445c1bb0f2e84fee697724f10b1
wiktoriasz0 <sabajamnik@gmail.com>
2022-01-29 10:39:11 +0100

    Dodany kontroler About

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace test_gita.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
